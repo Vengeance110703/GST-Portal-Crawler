fa7050f9cc0b176ee467c9052ef853010768be66
Yash Sheth <yashsheth66@gmail.com>
2024-08-07 02:29:27 +0530

    Enter GSTIN number

---
+++ crawler.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import requests
+import os
+
+# Global variables
+GSTIN = "27AAAAP0267H2ZN"
+file_name = "audio.mp3"
+
+chrome_options = webdriver.ChromeOptions()
+
+prefs = {
+    "download.default_directory": os.getcwd(),
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True,
+}
+
+chrome_options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://services.gst.gov.in/services/searchtp")
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located(
+        (By.XPATH, '//div[@class="dimmer-holder"][contains(@style, "display: none;")]')
+    )
+)
+
+gstin_text_box = driver.find_element(
+    By.XPATH, '//input[@placeholder="Enter GSTIN/UIN of the Taxpayer"]'
+)
+gstin_text_box.clear()
+gstin_text_box.send_keys(GSTIN)
+
+time.sleep(100)
+
+driver.quit()
