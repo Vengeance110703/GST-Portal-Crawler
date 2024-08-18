fa6886f062e16836f527658dc78907b390b9b98a
Yash Sheth <yashsheth66@gmail.com>
2024-08-19 03:59:53 +0530

    Further tests - whisper

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import whisper
 from os import path
 
-audio_folder_name = "audio"
-# download_dir = os.getcwd()
+# AUDIO_FILE_1 = path.join(path.dirname(path.realpath(__file__)), "audiocaptcha.mp3")
+# AUDIO_FILE_2 = path.join(path.dirname(path.realpath(__file__)), "audiocaptcha (1).mp3")
+# AUDIO_FILE_3 = path.join(path.dirname(path.realpath(__file__)), "audiocaptcha (2).mp3")
+# AUDIO_FILE_4 = path.join(path.dirname(path.realpath(__file__)), "audiocaptcha (3).mp3")
 
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audiocaptcha.mp3")
+# model = whisper.load_model("base.en")
+# result_1 = model.transcribe(AUDIO_FILE_1)
+# result_2 = model.transcribe(AUDIO_FILE_2)
+# result_3 = model.transcribe(AUDIO_FILE_3)
+# result_4 = model.transcribe(AUDIO_FILE_4)
 
-print(AUDIO_FILE)
+# print(result_1["text"])
+# print(result_2["text"])
+# print(result_3["text"])
+# print(result_4["text"])
 
-model = whisper.load_model("tiny")
-result = model.transcribe(AUDIO_FILE)
-
-print(result["text"])
+captcha = "A1 3 1 3 0"
+captcha = captcha.replace(" ", "")
+captcha = captcha.replace(",", "")
+if not captcha.isalnum():
+    print(captcha)
